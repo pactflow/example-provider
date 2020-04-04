8ed7e1512042f7049387790ed360c6cb7339c10e
Beth Skurrie <beth@bethesque.com>
2020-04-04 14:50:52 +1100

    feat: add CI

---
+++ product/product.pact.test.js
@@ -0,0 +1,57 @@
+const { Verifier } = require('@pact-foundation/pact');
+const controller = require('./product.controller');
+const Product = require('./product');
+
+// Setup provider server to verify
+const app = require('express')();
+const authMiddleware = require('../middleware/auth.middleware');
+app.use(authMiddleware);
+app.use(require('./product.routes'));
+const server = app.listen("8080");
+
+describe("Pact Verification", () => {
+  it("validates the expectations of ProductService", () => {
+    const opts = {
+        logLevel: "INFO",
+        providerBaseUrl: "http://localhost:8080",
+        provider: "pactflow-example-provider",
+        providerVersion: process.env.TRAVIS_COMMIT,
+        consumerVersionTag: ['master'],
+        providerVersionTag: process.env.TRAVIS_BRANCH,
+        pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
+        pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
+        pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
+        stateHandlers: {
+          "product with ID 10 exists": () => {
+            controller.repository.products = new Map([
+                ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+            ]);
+          },
+          "products exist": () => {
+            controller.repository.products = new Map([
+              ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+              ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+            ]);
+          },
+          "no products exist": () => {
+            controller.repository.products = new Map();
+          },
+          "product with ID 11 does not exist": () => {
+            controller.repository.products = new Map();
+          },
+        },
+        requestFilter: (req, res, next) => {
+          if (!req.headers["authorization"]) {
+            next();
+            return;
+          }
+          req.headers["authorization"] = `Bearer ${new Date().toISOString()}`;
+          next();
+        },
+    };
+
+    return new Verifier(opts).verifyProvider().finally(() => {
+        server.close();
+    });
+  })
+});
