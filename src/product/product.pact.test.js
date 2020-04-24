614baaaa70e18895bd2a6aeda3a4e6c72e9e2e0f
Beth Skurrie <beth@bethesque.com>
2020-04-24 10:38:32 +1000

    fix: provider state names

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -38,12 +38,12 @@ describe("Pact Verification", () => {
     }
 
     const stateHandlers = {
-      "product with ID 10 exists": () => {
+      "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
           ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
         ]);
       },
-      "product with ID 11 does not exist": () => {
+      "a product with ID 11 does not exist": () => {
         controller.repository.products = new Map();
       }
     }
