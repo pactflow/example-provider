f1c91ec9f6ab428f95e03cce27c9bd525ee37107
Matt Fellows <matt.fellows@onegeek.com.au>
2020-07-28 12:53:05 +1000

    test: support 'products exist' state

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -43,6 +43,11 @@ describe("Pact Verification", () => {
           ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
         ]);
       },
+      "products exist": () => {
+        controller.repository.products = new Map([
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+        ]);
+      },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
           ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
