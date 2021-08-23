14c6190b5c4c67a678c5a7cfdf967e6700ad3778
Mark Statkus <mstatkus@lululemon.com>
2021-08-23 11:09:50 -0700

    Adding the color back by popular demand

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -45,12 +45,13 @@ describe("Pact Verification", () => {
       },
       "products exist": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "blue")],
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "yellow")]
         ]);
       },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "green")]
         ]);
       },
       "a product with ID 11 does not exist": () => {
