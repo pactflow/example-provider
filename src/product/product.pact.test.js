482709b3c19bbdffab720601037f4fdd5a101414
LaiaPR <laia.pacheco@sngular.com>
2021-09-23 09:48:24 +0100

    Adding new field to Product

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -40,17 +40,17 @@ describe("Pact Verification", () => {
     const stateHandlers = {
       "products exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")]
         ]);
       },
       "products exist": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")]
         ]);
       },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")]
         ]);
       },
       "a product with ID 11 does not exist": () => {
--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,9 +4,9 @@ class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
+            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
+            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow")],
         ]);
     }
 
