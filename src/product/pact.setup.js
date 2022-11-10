d3b899e9e4654f67f764e58c55dcc41412aab6d5
Noor Hashem <noor.hashem@hotmail.com>
2022-11-10 14:08:58 -0500

    trigger

--- src/product/pact.setup.js
+++ src/product/pact.setup.js
@@ -5,7 +5,7 @@ const baseOpts = {
   logLevel: "INFO",
   providerBaseUrl: "http://localhost:8080",
   providerVersion: process.env.GIT_COMMIT,
-  providerVersionBranch: process.env.GIT_BRANCH, // the recommended way of publishing verification results with the branch property
+  providerVersionBranch: process.env.GIT_BRANCH, // the  recommended way of publishing verification results with the branch property
   verbose: process.env.VERBOSE === "true",
 };
 
