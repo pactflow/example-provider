8669a6d6a7e42b2d5e837677147a834a8d380230
steve.luther@omaticsoftware.com <steve.luther@omaticsoftware.com>
2023-07-18 04:49:50 -0600

    unbreaking shit in provider

--- src/product/pact.setup.js
+++ src/product/pact.setup.js
@@ -23,17 +23,17 @@ const setupServer = () => {
 const stateHandlers = {
   "products exists": () => {
     controller.repository.products = new Map([
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
     ]);
   },
   "products exist": () => {
     controller.repository.products = new Map([
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
     ]);
   },
   "a product with ID 10 exists": () => {
     controller.repository.products = new Map([
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
     ]);
   },
   "a product with ID 11 does not exist": () => {
--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -1,22 +1,21 @@
-const Product = require('./product');
+const Product = require("./product");
 
 class ProductRepository {
+  constructor() {
+    this.products = new Map([
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
+      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
+      ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow")],
+    ]);
+  }
 
-    constructor() {
-        this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
-        ]);
-    }
+  async fetchAll() {
+    return [...this.products.values()];
+  }
 
-    async fetchAll() {
-        return [...this.products.values()]
-    }
-
-    async getById(id) {
-        return this.products.get(id);
-    }
+  async getById(id) {
+    return this.products.get(id);
+  }
 }
 
 module.exports = ProductRepository;
