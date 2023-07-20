45f2aa8b1ff61823dccec455e6e6e36fd2aa5001
Matthew Robertson <matthew.robertson@skyscanner.net>
2023-07-20 11:54:08 +0100

    add more products

--- src/product/pact.setup.js
+++ src/product/pact.setup.js
@@ -11,14 +11,12 @@ const baseOpts = {
 
 
 const stateHandlers = {
-  "products exists": () => {
-    controller.repository.products = new Map([
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
-    ]);
-  },
   "products exist": () => {
     controller.repository.products = new Map([
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
       ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+      ["13", new Product("13", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+      ["12", new Product("12", "PERSONAL_LOAN", "Monzo", "v2")],
     ]);
   },
   "a product with ID 10 exists": () => {
